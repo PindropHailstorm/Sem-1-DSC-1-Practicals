19dbf3cf6d0caee58597fe411325116cdb281b3c
PindropHailstorm <119025864+PindropHailstorm@users.noreply.github.com>
2022-12-08 00:22:30 +0530

    To find the roots of a quadratic equation

---
+++ roots-quadratic-equation.py
@@ -0,0 +1,12 @@
+
+print("To find the roots of a quadratic equation of the form ax^2 + bx + c, please input the values of a, b and c"
+a = int(input("Input the value of a , a = "))
+b = int(input("Input the value of b , b = "))
+c = int(input("Input the value of c , c = "))
+
+root1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / 2 * a
+root2 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / 2 * a
+if root1!=root2:
+   print("The roots of the equation  are", root1, root2)
+else: print("The root of the equation is", root1)
+
