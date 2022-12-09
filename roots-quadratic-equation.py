3d07ae3e71b7156e8e6f93fc8eded6acafbf30a2
PindropHailstorm <119025864+PindropHailstorm@users.noreply.github.com>
2022-12-09 06:32:37 +0530

    Update roots-quadratic-equation.py

--- roots-quadratic-equation.py
+++ roots-quadratic-equation.py
@@ -5,7 +5,7 @@ b = int(input("Input the value of b , b = "))
 c = int(input("Input the value of c , c = "))
 
 root1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / 2 * a
-root2 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / 2 * a
+root2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / 2 * a
 if root1!=root2:
    print("The roots of the equation  are", root1, root2)
 else: print("The root of the equation is", root1)
